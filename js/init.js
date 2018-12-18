6f701a136140ff9f66294f35799ee7e5e7b25502
Bruv Weird <thulani.ncube21@yahoo.com>
2018-12-18 21:55:49 +0200

    First Commit

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.collapsible').collapsible();
+    $('.modal').modal();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
